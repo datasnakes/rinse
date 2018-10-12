a35342ad6b148f7d730c208935d3dff4f2267b61
ksbystrom@gmail.com <ksbystrom@gmail.com>
2018-10-12 13:31:58 -0700

    First commit of rinse.py file. Not much there yet.

---
+++ rinse.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install pylint')
+
+
+# In[2]:
+
+
+get_ipython().system('pip install argparse')
+
+
+# In[3]:
+
+
+get_ipython().system('pip apt-get install r-base')
+
+
+# In[ ]:
+
+
+def main()
+    parser = argparse.ArgumentParser(description = "Download any version of R from source")
+    parser.add_argument("-version")
+
+
+# In[ ]:
+
+
+if __name__=='__main__':
+    main()
+

f0cf9894e0f89a01b269dd78afe48720015b3101
sdhutchins <sdhutchins@outlook.com>
2018-10-14 13:33:10 -0500

    Added simple cli for installing R

--- rinse/rinse.py
+++ rinse/rinse.py
@@ -1,8 +1,13 @@
 import click
 
+from rinse.core import install_r
+
+# TODO Convert install from -i to just install.
 @click.command()
-@click.option('--install', '-i', help="Will print verbose messages.")
+@click.option('--install', '-i', help="Install R.")
 @click.option('--version', '-v', default='3.5.0',
               help="Select the version of R to install.")
-def cli(install, version):
-    click.echo("Hello World")
+@click.option('--path', '-p', default='~/bin',
+              help="Select an installation path for R.")
+def cli(install, version, path):
+    install_r(version, path)

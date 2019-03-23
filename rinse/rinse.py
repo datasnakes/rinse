1df998218a9a1e95e71fcf0afd6d5725abbe32b9
grabear <robgilmore127@gmail.com>
2019-03-23 18:23:15 -0500

    Reworked rinse CLI and began integrating InstallR class.

--- rinse/rinse.py
+++ rinse/rinse.py
@@ -1,17 +1,14 @@
-from rinse.utils import import_temp
 from rinse import installr
 from pkg_resources import resource_filename
-from tempfile import mkdtemp
-from subprocess import run
 from os.path import expanduser, abspath
 import click
-
+from rinse.core import InstallR
 
 INSTALLR = resource_filename(installr.__name__, "installr.sh")
 
 
 @click.command()
-@click.option("--path", "-p", default="~/beRi_installs",
+@click.option("--path", "-p", default="~/.rinse",
               help="Select an installation path for R.")
 @click.option("--version", "-v", default="latest",
               help="Select the version of R to install.")
@@ -27,22 +24,26 @@ def rinse(version, path, repos):
     else:
         major_version = version[0:1]
         url = f"{repos}/src/base/R-{major_version}/R-{version}.tar.gz"
+    method = None
+
+    rinse = InstallR(path=path, version=version, method=method, url=url)
+    rinse.install()
 
     # Create temporary directory and files
-    tmp_dir = mkdtemp()
-    script_path = f"{tmp_dir}/script.sh"
-    script = import_temp(INSTALLR)
-    stdout = f"{tmp_dir}/stdout.txt"
-    stderr = f"{tmp_dir}/stderr.txt"
-    updated_script = script.safe_substitute(version=version, path=path,
-                                            url=url, tmp_dir=tmp_dir,
-                                            stdout=stdout, stderr=stderr)
-    with open(script_path, "w") as out:
-        out.write(updated_script)
-
-    # Run updated installation script
-    click.echo(f"$ bash {script_path}")
-    install_proc = run(["bash", script_path])
-    if install_proc.returncode != 0:
-        click.echo(f"Check the following files for installation output:\n"
-                   f"  - {stdout}\n  - {stderr}")
+    # tmp_dir = mkdtemp()
+    # script_path = f"{tmp_dir}/script.sh"
+    # script = import_temp(INSTALLR)
+    # stdout = f"{tmp_dir}/stdout.txt"
+    # stderr = f"{tmp_dir}/stderr.txt"
+    # updated_script = script.safe_substitute(version=version, path=path,
+    #                                         url=url, tmp_dir=tmp_dir,
+    #                                         stdout=stdout, stderr=stderr)
+    # with open(script_path, "w") as out:
+    #     out.write(updated_script)
+    #
+    # # Run updated installation script
+    # click.echo(f"$ bash {script_path}")
+    # install_proc = run(["bash", script_path])
+    # if install_proc.returncode != 0:
+    #     click.echo(f"Check the following files for installation output:\n"
+    #                f"  - {stdout}\n  - {stderr}")

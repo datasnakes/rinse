f7b622b8b373a0179be9f7f2ce17f6e2ff2e1d2c
Rob Gilmore <robgilmore127@gmail.com>
2019-03-23 20:31:48 -0500

    Added local method and began code for methods and classes.

--- rinse/core.py
+++ rinse/core.py
@@ -2,6 +2,9 @@ class InstallR(object):
 
     def __init__(self, path, version, method="source", url=None):
         self.method = method  # source for now spack for later
+        self.path = path
+        self.version = version
+        self.url = url
         self.install()
 
     def install(self):
@@ -9,16 +12,23 @@ class InstallR(object):
             self.use_source()
         elif self.method == "spack":
             self.use_spack()
+        elif self.method == "local":
+            self.use_local()
+
+    def use_local(self):
+        pass
 
     def use_source(self):
         pass
 
     def use_spack(self):
-        pass
+        raise NotImplementedError("Installation with spack is not supported at this time.")
 
 
 class LInstallR(InstallR):
-    pass
+
+    def __init__(self):
+        super().__init__()
 
 
 class MacInstall(InstallR):

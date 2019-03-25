17563f259c06685f8a49010eaca58649dfdd7688
Rob Gilmore <robgilmore127@gmail.com>
2019-03-25 16:55:17 -0500

    Removed deprecated code from rinse CLI.

--- rinse/rinse.py
+++ rinse/rinse.py
@@ -1,5 +1,3 @@
-from rinse import installr
-from pkg_resources import resource_filename
 from os import name as osname
 from sys import platform as sysplat
 import click
@@ -45,21 +43,3 @@ def rinse(path, name, install, repos, method, init, config_file, config_help):
         else:
             raise OSError("rinse does not support the %s operating system at this time." % sysplat)
 
-    # Create temporary directory and files
-    # tmp_dir = mkdtemp()
-    # script_path = f"{tmp_dir}/script.sh"
-    # script = import_temp(INSTALLR)
-    # stdout = f"{tmp_dir}/stdout.txt"
-    # stderr = f"{tmp_dir}/stderr.txt"
-    # updated_script = script.safe_substitute(version=version, path=path,
-    #                                         url=url, tmp_dir=tmp_dir,
-    #                                         stdout=stdout, stderr=stderr)
-    # with open(script_path, "w") as out:
-    #     out.write(updated_script)
-    #
-    # # Run updated installation script
-    # click.echo(f"$ bash {script_path}")
-    # install_proc = run(["bash", script_path])
-    # if install_proc.returncode != 0:
-    #     click.echo(f"Check the following files for installation output:\n"
-    #                f"  - {stdout}\n  - {stderr}")

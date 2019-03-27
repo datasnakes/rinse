dc548a65dac74514abefeeac39b6eeb766deaeeb
Rob Gilmore <robgilmore127@gmail.com>
2019-03-27 13:17:44 -0500

    Potential bug fix for --clear_config.

--- rinse/core.py
+++ rinse/core.py
@@ -139,7 +139,7 @@ class LInstallR(InstallR):
 
     def clear_tmp_dir(self, version=None):
         # Set up the temporary directory for installation
-        if len(version) == 1 and version[0] == "all":
+        if version == "all":
             rmtree(str(self.tmp_path))
             self.tmp_path.mkdir(parents=True)
         elif len(version) >= 1:

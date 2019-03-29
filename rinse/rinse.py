1a085960fc89f6908aa80ffff0047892b4ab95fd
Rob Gilmore <robgilmore127@gmail.com>
2019-03-29 14:53:22 -0500

    Updated configure configure command.

--- rinse/rinse.py
+++ rinse/rinse.py
@@ -3,40 +3,34 @@ from os import listdir, chdir, mkdir, symlink, remove, environ
 from rinse.core import BaseInstallR
 from rinse.utils import get_system_installer
 
+
 @click.group()
-@click.option("--global", "-g", "glbl", default=None,
-              help="Select the version of R available to your global environment.")
-@click.option("--repos", "-r", default="http://cran.rstudio.com")
+# @click.option("--global", "-g", "glbl", default=None,
+#               help="Select the version of R available to your global environment.")
 @click.option("--source", "method", flag_value="source", default=True, show_default=True)
 @click.option("--spack", "method", flag_value="spack", show_default=True)
 @click.option("--local", "method", flag_value="local", show_default=True)
 @click.option("--path", "-p", default="~/.beRi",
-              help="Select a relative installation path for rinse.", show_default=True)
+              help="An absolute installation path for rinse.", show_default=True)
 @click.option("--name", "-n", default=".rinse",
-              help="Select a name for the installation directory for R.", show_default=True)
-@click.option("--config_file", default=None,
-              help="A text file for sending commands to the configure script that"
-                   " configures R to adapt to many kinds of systems.", show_default=True)
-@click.option("--config_help", default=False,
-              help="Display the help message for configuring R.", show_default=True)
-@click.option("--config_clear", "-c", default=list(["all"]), multiple=True,
-              help="Remove any files associated with previous attempts to install R.", show_default=True)
+              help="A directory name for rinse.", show_default=True)
+# @click.option("--config_clear", "-c", default=list(["all"]), multiple=True,
+#               help="Remove any files associated with previous attempts to install R.", show_default=True)
+@click.option("--repos", "-r", default="http://cran.rstudio.com",
+              help="The repository to use for downloading source files.")
 @click.pass_context
-def rinse(ctx, glbl, repos, method, path, name, config_file, config_help, config_clear):
+def rinse(ctx, repos, method, path, name, config_clear):
     ctx.ensure_object(dict)
     ctx.obj['path'] = path
     ctx.obj['name'] = name
     ctx.obj['method'] = method
+    ctx.obj['repos'] = repos
     if path != "~/.beRi":
         raise NotImplementedError("Rinse only supports installing into the home directory at this time.")
 
     # Get the system dependent installation class
     installR = get_system_installer()
-    Rhandle = installR(glbl=glbl, path=path, install=install, repos=repos, method=method, name=name,
-                       config_file=config_file, config_help=config_help, config_clear=config_clear, init=False)
-    ctx.obj['Rinstall'] = Rhandle
-    if install is not None:
-        Rhandle.installer()
+    ctx.obj['installR'] = installR
 
 
 @rinse.command(help="Initialize rinse using the /<path>/<name>.")
@@ -46,27 +40,51 @@ def init(ctx):
     BaseInstallR(path=ctx.obj['path'], name=ctx.obj['name'], init=True)
 
 
-@rinse.command(context_settings=dict(
+@rinse.command(name="global")
+@click.pass_context
+def _global(ctx):
+    pass
+
+
+@rinse.group()
+# @click.option("--version", default=None,
+#               help="Select the version of R to install.", show_default=True)
+@click.argument('version', default="latest")
+@click.pass_context
+def install(ctx, version):
+    ctx.obj['version'] = version
+    ctx.invoke(configure, configure_opts=version)
+    ctx.invoke(make)
+
+
+@install.command(context_settings=dict(
     ignore_unknown_options=True,
     allow_extra_args=True,
     help_option_names=['--chelp'],
 ))
-@click.argument('configure_opts', nargs=-1, type=click.UNPROCESSED)
+@click.argument('configure_opts', default="latest", nargs=-1, type=click.UNPROCESSED)
 @click.pass_context
 def configure(ctx, configure_opts):
-    Rinstall = ctx.obj['Rinstall']
-    rinse_bin = Rinstall.source_setup()
+    if configure_opts == "latest":  # No version is specified via install() or configure()
+        version = configure_opts
+        configure_opts = None
+    elif len(configure_opts) > 6:  # From using configure() via "rinse install configure 3.3.3 --prefix=etc."
+        version = configure_opts[0:5]
+        configure_opts = configure_opts[6:]
+    else:  # From using configure() via "rinse install 3.3.3"
+        version = configure_opts
+        configure_opts = None
+
+    installR = ctx.obj['installR']
+    installR = installR(version=version, path=ctx.obj['path'], name=ctx.obj['name'], method="source",
+                        repos=ctx.obj['repos'])
+    src_file_path = installR.source_download()
+    rinse_bin = installR.source_setup(src_file_path=src_file_path)
     chdir(str(rinse_bin))
-    Rinstall.source_configure(rinse_bin=rinse_bin, configure_opts=configure_opts)
+    installR.source_configure(rinse_bin=rinse_bin, configure_opts=configure_opts)
 
 
 @rinse.command()
 @click.pass_context
 def make(ctx):
-    pass
-
-
-@rinse.command()
-@click.pass_context
-def install(ctx):
     pass
